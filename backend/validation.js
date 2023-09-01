1968527a52efdfeaf310151683e8e06178862b6d
Lokendra Bhat <lokendrabhat.2022@gmail.com>
2023-09-01 22:17:49 +0545

    finished

--- backend/validation.js
+++ backend/validation.js
@@ -5,6 +5,8 @@ function doValidate() {
     var phone = document.getElementById("phno");
     var Email = document.getElementById("email");
     var dob = document.getElementById("dob");
+    var cit_no = document.getElementById("CNo");
+
 
     // Full Name validation
     if (name.value == "" || name.value.length < 3 || name.value.charAt(0) == " " || !isNaN(name.value)) {
@@ -57,13 +59,50 @@ function doValidate() {
     }
 
     // DateOfBirth validation
-    if (dob.value == "") {
+    if (age < 60) {
+        alert("You must be 60+ to register!");
+        return false;
+    }
+    if (dob.value == "" ) {
         dob.focus();
         dob.style.border = "1px solid Red";
         return false;
     }
     else {
         dob.style.border = "1px solid transparent";
+    }
+    
+    // validate for 60+ age
+    // Age validation
+    var today = new Date();
+    var birthDate = new Date(dob.value);
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) 
+    {
+        age--;
+    }
+    if(age < 60)
+    {
+        alert("You must be 60+ to register!");
+        dob.focus();
+        return false;
+    }
+    else if(age > 100)
+    {
+        alert("You must be verify for age 100+ to register!");
+        dob.focus();
+        return false;
+    }
+
+    // Citizen number validation
+    if (cit_no.value == "" || isNaN(cit_no.value)) {
+        cit_no.focus();
+        cit_no.style.border = "1px solid Red";
+        return false;
+    }
+    else {
+        cit_no.style.border = "1px solid transparent";
     }
 
     // term and conditions validation
